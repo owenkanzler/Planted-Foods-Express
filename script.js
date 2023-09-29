0a6e308970c195d6ff040813bd81e2488877a501
owenkanzler <136407277+owenkanzler@users.noreply.github.com>
2023-09-28 19:54:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+const modal2 = document.querySelector(".modal2");
+const openModal2 = document.querySelector(".open-button2");
+const closeModal2 = document.querySelector(".close-button2");
+
+openModal2.addEventListener("click", () => {
+  modal2.showModal();
+});
+
+closeModal2.addEventListener("click", () => {
+  modal2.close();
+});
