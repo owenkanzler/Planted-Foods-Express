fb4fe74ce09eaa77a3e9760c513e49489cafb109
Owen Kanzler <136407277+owenkanzler@users.noreply.github.com>
2023-11-16 11:24:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const modal = document.querySelector(".modal");
-const openModal = document.querySelector(".open-button");
-const closeModal = document.querySelector(".close-button");
+// const modal = document.querySelector(".modal");
+// const openModal = document.querySelector(".open-button");
+// const closeModal = document.querySelector(".close-button");
 
-openModal.addEventListener("click", () => {
-  modal.showModal();
-});
+// openModal.addEventListener("click", () => {
+//   modal.showModal();
+// });
 
-closeModal.addEventListener("click", () => {
-  modal.close();
-});
+// closeModal.addEventListener("click", () => {
+//   modal.close();
+// });
 
-const modal2 = document.querySelector(".modal2");
-const openModal2 = document.querySelector(".open-button2");
-const closeModal2 = document.querySelector(".close-button2");
+// const modal2 = document.querySelector(".modal2");
+// const openModal2 = document.querySelector(".open-button2");
+// const closeModal2 = document.querySelector(".close-button2");
 
-openModal2.addEventListener("click", () => {
-  modal2.showModal();
-});
+// openModal2.addEventListener("click", () => {
+//   modal2.showModal();
+// });
 
-closeModal2.addEventListener("click", () => {
-  modal2.close();
-});
+// closeModal2.addEventListener("click", () => {
+//   modal2.close();
+// });
